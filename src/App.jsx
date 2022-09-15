dbfc86b6f20c86e6fb64ff1bd8cc555fa147b545
AdalObregon <adalobregoon@gmail.com>
2022-09-14 21:36:27 -0500

    base project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-import Title from './components/Title';
 
 function App() {
-  const [counter, setCounter] = useState(0);
 
   return (
     <>
       <div className='App'>
-      <Title />
-      
       </div>
     </>
   );
